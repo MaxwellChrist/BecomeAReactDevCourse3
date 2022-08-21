3c6f2582fe985fc1e9e52268e20e5687fc03837b
MaxwellChrist <ribthulu@gmail.com>
2022-08-21 16:14:46 -0400

    installed mongodb database and created my own locally to store and show RESTful API requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,45 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { MongoClient } from 'mongodb'
 
-const articleInfo = {
-    'a': {
-        upvotes: 0,
-        comments: [],
-    },
-    'b': {
-        upvotes: 0,
-        comments: [],
-    },
-    'c': {
-        upvotes: 0,
-        comments: [],
-    },
-    'd': {
-        upvotes: 0,
-        comments: [],
-    },
-}
+// const articleInfo = {
+//     'a': {
+//         upvotes: 0,
+//         comments: [],
+//     },
+//     'b': {
+//         upvotes: 0,
+//         comments: [],
+//     },
+//     'c': {
+//         upvotes: 0,
+//         comments: [],
+//     },
+//     'd': {
+//         upvotes: 0,
+//         comments: [],
+//     },
+// }
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true})
+        const db = client.db('my-blog')
+    
+        const articleInfo = await db.collection('articles').findOne({name: articleName})
+        res.status(200).json(articleInfo)
+    
+        client.close()
+    } catch(err) {
+        res.status(500).json({message: "Error connecting to database", err})
+    }
+})
 
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
