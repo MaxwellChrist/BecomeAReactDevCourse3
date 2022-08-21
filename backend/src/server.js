27b401aa162586422c95f789c0ffb9a2bfa344bc
MaxwellChrist <ribthulu@gmail.com>
2022-08-21 18:27:06 -0400

    refactored backend to be more DRY and to use get and post request with mongodb database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,57 +1,59 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import { MongoClient } from 'mongodb'
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 
-// const articleInfo = {
-//     'a': {
-//         upvotes: 0,
-//         comments: [],
-//     },
-//     'b': {
-//         upvotes: 0,
-//         comments: [],
-//     },
-//     'c': {
-//         upvotes: 0,
-//         comments: [],
-//     },
-//     'd': {
-//         upvotes: 0,
-//         comments: [],
-//     },
-// }
+const app = express();
+app.use(bodyParser.json());
 
-const app = express()
+const setupDB = async (item, errorResponse) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
 
-app.use(bodyParser.json())
+        await item(db)
+    
+        client.close();
+    } catch (error) {
+        errorResponse.status(500).json({ message: 'Error connecting to db', error });
+    }
+}
 
 app.get('/api/articles/:name', async (req, res) => {
-    try {
+    setupDB(async (item) => {
         const articleName = req.params.name;
-
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true})
-        const db = client.db('my-blog')
-    
-        const articleInfo = await db.collection('articles').findOne({name: articleName})
-        res.status(200).json(articleInfo)
-    
-        client.close()
-    } catch(err) {
-        res.status(500).json({message: "Error connecting to database", err})
-    }
+        const articleInfo = await item.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+    }, res)
 })
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
-    articleInfo[articleName].upvotes += 1
-    res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvote(s)!`)
-})
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    setupDB(async (item) => {
+        const articleName = req.params.name;
+        const articleInfo = await item.collection('articles').findOne({ name: articleName });
+        await item.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+        const updatedArticleInfo = await item.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo)
+    }, res)
+});
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
-    const {username, text} = req.body
+    const { username, text } = req.body;
     const articleName = req.params.name;
-    articleInfo[articleName].comments.push({username, text})
-    res.status(200).send(articleInfo[articleName])
-})
 
-app.listen(3000, () => console.log("listening on port 3000"))
+    setupDB(async (item) => {
+        const articleInfo = await item.collection('articles').findOne({name: articleName})
+        await item.collection('articles').updateOne({ name: articleName}, {
+            '$set': {
+                comments: articleInfo.comments.concat({text})
+            },
+        })
+        const updatedArticleInfo = await item.collection('articles').findOne({name: articleName})
+        res.status(200).json(updatedArticleInfo)
+    }, res)
+});
+
+app.listen(8000, () => console.log('Listening on port 8000'));
