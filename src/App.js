dcf9ea150bcc9ced390da689616109c04d171a19
MaxwellChrist <ribthulu@gmail.com>
2022-08-21 09:54:20 -0400

    setup project. Added pages folder to hold my four created components (home, about, article list, and article). Updated app.js to link to correct routes for each component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticleList from './pages/ArticleList';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/article-list" element={<ArticleList />}/>
+            <Route path="/article" element={<Article />}/>
+        </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/ArticleList.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const ArticleList = () => {
+    return (
+        <>
+            <h1>Articles</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi 
+                nullam vehicula ipsum a arcu cursus. Massa tempor nec feugiat nisl 
+                pretium fusce id. Risus quis varius quam quisque id diam vel quam. 
+                Elit sed vulputate mi sit amet mauris commodo. Non quam lacus 
+                suspendisse faucibus interdum posuere lorem. Adipiscing elit ut 
+                aliquam purus sit amet luctus venenatis lectus. Aenean sed adipiscing 
+                diam donec adipiscing tristique risus nec. Accumsan lacus vel facilisis 
+                volutpat est. Vulputate ut pharetra sit amet aliquam.
+            </p>
+            <p>    
+                Pellentesque dignissim enim sit amet venenatis urna. Sem integer vitae 
+                justo eget magna fermentum. Tortor vitae purus faucibus ornare suspendisse. 
+                Pharetra et ultrices neque ornare aenean euismod elementum nisi quis. 
+                Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Commodo 
+                quis imperdiet massa tincidunt. Magnis dis parturient montes nascetur 
+                ridiculus mus. Aliquam eleifend mi in nulla posuere sollicitudin aliquam 
+                ultrices. Eu tincidunt tortor aliquam nulla. In nibh mauris cursus mattis 
+                molestie. Euismod nisi porta lorem mollis aliquam ut. Tortor vitae purus 
+                faucibus ornare suspendisse.
+            </p>
+            <p>   
+                Sed nisi lacus sed viverra tellus. Senectus et netus et malesuada. 
+                Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Faucibus 
+                nisl tincidunt eget nullam non nisi est sit amet. Vitae proin sagittis nisl 
+                rhoncus mattis rhoncus urna neque viverra. Dolor sit amet consectetur 
+                adipiscing elit pellentesque habitant. Quisque egestas diam in arcu cursus 
+                euismod quis viverra. In metus vulputate eu scelerisque felis imperdiet proin. 
+                Interdum consectetur libero id faucibus nisl. Metus aliquam eleifend mi in 
+                nulla posuere sollicitudin. Cum sociis natoque penatibus et magnis dis parturient. 
+                Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Dignissim suspendisse 
+                in est ante.
+            </p>
+        </>
+    )
+}
+
+export default ArticleList
