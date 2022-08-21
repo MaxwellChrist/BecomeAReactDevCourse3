dc5db1192c4e79282376ccb0bb3477a5bab003dd
MaxwellChrist <ribthulu@gmail.com>
2022-08-21 11:05:12 -0400

    added article content component to store mock article data and map over it in article component

---
+++ src/components/ArticleContent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const articles = [
+    {
+        name: "fdsafsd",
+        title: "abc",
+        content: [
+            `sdfsdafdasf`
+        ]
+    },
+    {
+        name: "sdfsdaf",
+        title: "def",
+        content: [
+            `sdafdsafsdafdsafeasdfsd`
+        ]
+    }
+]
+
+export default articles
