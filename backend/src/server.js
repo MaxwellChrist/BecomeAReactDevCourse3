6e6a3bde8e2f86d472ac94cba5d64fc5e2ea6110
MaxwellChrist <ribthulu@gmail.com>
2022-08-21 14:44:40 -0400

    built post request to handle mock comments

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,19 @@ import bodyParser from 'body-parser'
 const articleInfo = {
     'a': {
         upvotes: 0,
+        comments: [],
     },
     'b': {
         upvotes: 0,
+        comments: [],
     },
     'c': {
         upvotes: 0,
+        comments: [],
     },
     'd': {
         upvotes: 0,
+        comments: [],
     },
 }
 
@@ -24,6 +28,13 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
     articleInfo[articleName].upvotes += 1
     res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvote(s)!`)
+})
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const {username, text} = req.body
+    const articleName = req.params.name;
+    articleInfo[articleName].comments.push({username, text})
+    res.status(200).send(articleInfo[articleName])
 })
 
 app.listen(3000, () => console.log("listening on port 3000"))
